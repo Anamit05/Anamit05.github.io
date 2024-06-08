c7c7eb05ddc25d1f5ce587dfb36f100132547ea7
Anamit05 <markkennethposadas05@gmail.com>
2024-06-08 09:56:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function hitLikes1() {
   countLikes1.textContent = totalLikes1.toString()
 }
 btnLike1.addEventListener("click",hitLikes1)
+function hitLikes2() {
+    let totalLikes2 = parseInt(countLikes2.value) + 1
+  countLikes2.textContent = totalLikes2.toString()
+}
+btnLike2.addEventListener("click",hitLikes2)
